da3198e137b39bb29df37d8f483326998007a86f
Arsalan Habib <71399565+arsalanhabib01@users.noreply.github.com>
2026-02-10 09:59:44 +0100

    Added the "comment on movie" functionality.

---
+++ MVC-Movie/Data/Migrations/20260208134428_AddMovieComments.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_Movie.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMovieComments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MovieComments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CommentText = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieComments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MovieComments_Movie_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movie",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieComments_MovieId",
+                table: "MovieComments",
+                column: "MovieId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovieComments");
+        }
+    }
+}
---
+++ MVC-Movie/Models/MovieComment.cs
@@ -0,0 +1,13 @@
+﻿namespace MVC_Movie.Models
+{
+    public class MovieComment
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+        public string UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string CommentText { get; set; }
+    }
+}
